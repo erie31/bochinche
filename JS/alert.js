0623b8a82d7dcb40d46c7323e31f5feea70dc8ab
Erié <erie31@gmail.com>
2022-06-27 15:56:16 -0300

    Incluyendo libreria SweetAlert2

---
+++ JS/alert.js
@@ -0,0 +1,30 @@
+Swal.fire({
+	title:"¡Mes del TORO!",
+	text:"Suma el Toro Mecanico a tu COMBO",
+	icon: undefined,
+	footer:"50%OFF",
+	width:"300px",
+	padding:"1.5rem",
+	background:"#fff",
+	grow: false,
+	backdrop: false,
+	timer:15000,
+	timerProgressBar: true,
+	toast:false,
+	position:"bottom-end",
+	allowOutsideClick:false,
+	allowEscapeKey: false,
+	stopKeydownPropagation:false,
+
+	showConfirmButton: false,
+	confirmButtonColor:false,
+	confirmButtonAriaLabel:true,
+
+	showCloseButton:true,
+	closeButtonAriaLabel:"Cerrar",
+
+
+	imageUrl:"https://www.publicdomainpictures.net/pictures/140000/nahled/bull-1445811812MQG.jpg",
+	imageWidth:"50%",
+	imageAlt:"imagen de toro",
+});
